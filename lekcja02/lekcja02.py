bd86500120314df2cebba852daa3b310c8efa49b
xmedo1 <xmedo117@proton.me>
2024-10-16 11:56:43 +0200

    2.12-16

--- lekcja02/lekcja02.py
+++ lekcja02/lekcja02.py
@@ -1,9 +1,10 @@
 # 2.10
 line="""Lorem ipsum dolor       sit amet.
  In amet consequatur                 ut amet praesentium ut dicta autem ea   voluptatibus autem et dolores saepe.
- Ut repellendus aspernatur nam           atque amet est quaerat earum et tempore galisum."""
-
-print("2.10:\nIlosc wyrazow: " + str(len(line.split())))
+ Ut repellendus aspernatur nam           atque amet est quaerat earum et tempore galisum.
+ GvR"""
+print("Tekst:\n" + line)
+print("\n2.10:\nIlosc wyrazow: " + str(len(line.split())))
 
 # 2.11
 word="PrzykladowyTekst"
@@ -16,3 +17,25 @@ ostatnieZnaki="".join(slowo[-1] for slowo in line.split())
 
 print("\n2.12:\nSlowo z pierwszych znakow: " + pierwszeZnaki)
 print("Slowo z ostatnich znakow: " + ostatnieZnaki)
+
+# 2.13
+sumaDlgWyrazow = sum(len(slowo) for slowo in line.split())
+
+print("\n2.13\nLaczna dlugosc napisow w line: " + str(sumaDlgWyrazow))
+
+# 2.14
+najdluzszeSlowo = max(line.split(),key=len)
+
+print("\n2.14\nNajdluzsze slowo w line: " + najdluzszeSlowo)
+print("Dlugosc tego slowa: " + str(len(najdluzszeSlowo)))
+
+# 2.15
+L=list(range(1,10))
+print("\n2.15\nLista: " + str(L))
+print("Slowo z elementow listy: " + str("".join(str(liczba) for liczba in L)))
+
+# 2.16
+kopia = str(line)
+kopia = kopia.replace("GvR","Guido van Rossum")
+
+print("\n2.16\nTekst z zastapionym GvR:\n" + kopia)
