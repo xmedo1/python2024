ebb6cd5a11e98972f295de1cb9c602c06bec4080
xmedo1 <xmedo117@proton.me>
2024-10-16 12:28:25 +0200

    2.10-19

--- lekcja02/lekcja02.py
+++ lekcja02/lekcja02.py
@@ -39,3 +39,26 @@ kopia = str(line)
 kopia = kopia.replace("GvR","Guido van Rossum")
 
 print("\n2.16\nTekst z zastapionym GvR:\n" + kopia)
+
+# 2.17
+slowa = line.split()
+posortowaneAlfabetycznie = sorted(slowa,key=str.lower)
+posortowaneDlugoscia = sorted(slowa,key=len)
+
+print("\n2.17\nTekst posortowany alfabetycznie:\n" + str(" ".join(posortowaneAlfabetycznie)))
+print("Tekst posortowany pod wzgledem dlugosci slow:\n" + str(" ".join(posortowaneDlugoscia)))
+
+# 2.18
+
+duzaLiczba = 372175000923810205017050024812094
+iloscZer = str(duzaLiczba).count('0')
+
+print("\n2.18\nLiczba: " + str(duzaLiczba))
+print("Ilosc zer w tej liczie: " + str(iloscZer))
+
+# 2.19
+L = [312,61,38,8,395,613,666,373,213,7,633,991,123,321]
+# wyraz oddzielony kropkami dla lepszej czytelnosci
+wyraz = ".".join(str(liczba).zfill(3) for liczba in L)
+
+print("\n2.19\nWyraz z 3-cyfrowych blokow: " + str(wyraz))
