6125a6245fa84017deaf0a7dbd272b885a9f9183
xmedo1 <xmedo117@proton.me>
2024-11-25 13:14:40 +0100

    rectangles base

---
+++ lekcja08/rectangles.py
@@ -0,0 +1,65 @@
+from lekcja06.points import Point
+
+
+class Rectangle:
+    """Klasa reprezentująca prostokąty na płaszczyźnie."""
+
+    def __init__(self, x1, y1, x2, y2):
+        if x1 >= x2 or y1 >= y2:
+            raise ValueError("Chcemy, aby x1 < x2, y1 < y2.")
+        self.pt1 = Point(x1, y1)
+        self.pt2 = Point(x2, y2)
+
+    def __str__(self):
+        return f"[({self.pt1.x}, {self.pt1.y}), ({self.pt2.x}, {self.pt2.y})]"
+
+    def __repr__(self):
+        return f"Rectangle({self.pt1.x}, {self.pt1.y}, {self.pt2.x}, {self.pt2.y})"
+
+    def __eq__(self, other):
+        return self.pt1 == other.pt1 and self.pt2 == other.pt2
+
+    def __ne__(self, other):
+        return not self == other
+
+    def center(self):  # zwraca środek prostokąta
+        center_x = (self.pt1.x + self.pt2.x) / 2
+        center_y = (self.pt1.y + self.pt2.y) / 2
+        return Point(center_x, center_y)
+
+    def area(self):
+        len_x = abs(self.pt1.x - self.pt2.x)
+        len_y = abs(self.pt1.y - self.pt2.y)
+        return len_x * len_y
+
+    def move(self, x, y):
+        self.pt1.x += x
+        self.pt1.y += y
+        self.pt2.x += x
+        self.pt2.y += y
+
+    def intersection(self, other):
+        x1 = max(self.pt1.x, other.pt1.x)
+        y1 = max(self.pt1.y, other.pt1.y)
+        x2 = min(self.pt2.x, other.pt2.x)
+        y2 = min(self.pt2.y, other.pt2.y)
+        if x1 < x2 and y1 < y2:
+            return Rectangle(x1, y1, x2, y2)
+        else:
+            return None
+
+    def cover(self, other):
+        x1 = min(self.pt1.x, other.pt1.x)
+        y1 = min(self.pt1.y, other.pt1.y)
+        x2 = max(self.pt2.x, other.pt2.x)
+        y2 = max(self.pt2.y, other.pt2.y)
+        return Rectangle(x1, y1, x2, y2)
+
+    def make4(self):
+        center = self.center()
+        return (
+            Rectangle(self.pt1.x, self.pt1.y, center.x, center.y),
+            Rectangle(center.x, self.pt1.y, self.pt2.x, center.y),
+            Rectangle(self.pt1.x, center.y, center.x, self.pt2.y),
+            Rectangle(center.x, center.y, self.pt2.x, self.pt2.y)
+        )
