d8df64daae771bb4a0f3db436a31ef93749172ab
xmedo1 <xmedo117@proton.me>
2024-11-25 16:31:56 +0100

    lekcja08 final

--- lekcja08/rectangles.py
+++ lekcja08/rectangles.py
@@ -10,6 +10,13 @@ class Rectangle:
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
+    @classmethod
+    def from_points(cls, points):
+        if len(points) != 2:
+            raise ValueError("Podales wiecej niz dwa punkty.")
+        pt1, pt2 = points
+        return cls(pt1.x, pt1.y, pt2.x, pt2.y)
+
     def __str__(self):
         return f"[({self.pt1.x}, {self.pt1.y}), ({self.pt2.x}, {self.pt2.y})]"
 
@@ -22,11 +29,13 @@ class Rectangle:
     def __ne__(self, other):
         return not self == other
 
+    @property
     def center(self):  # zwraca środek prostokąta
         center_x = (self.pt1.x + self.pt2.x) / 2
         center_y = (self.pt1.y + self.pt2.y) / 2
         return Point(center_x, center_y)
 
+    @property
     def area(self):
         len_x = abs(self.pt1.x - self.pt2.x)
         len_y = abs(self.pt1.y - self.pt2.y)
@@ -56,10 +65,50 @@ class Rectangle:
         return Rectangle(x1, y1, x2, y2)
 
     def make4(self):
-        center = self.center()
+        center = self.center
         return (
             Rectangle(self.pt1.x, self.pt1.y, center.x, center.y),
             Rectangle(center.x, self.pt1.y, self.pt2.x, center.y),
             Rectangle(self.pt1.x, center.y, center.x, self.pt2.y),
             Rectangle(center.x, center.y, self.pt2.x, self.pt2.y)
         )
+
+    @property
+    def top(self):
+        return self.pt2.y
+
+    @property
+    def left(self):
+        return self.pt1.x
+
+    @property
+    def bottom(self):
+        return self.pt1.y
+
+    @property
+    def right(self):
+        return self.pt2.x
+
+    @property
+    def width(self):
+        return self.pt2.x - self.pt1.x
+
+    @property
+    def height(self):
+        return self.pt2.y - self.pt1.y
+
+    @property
+    def topleft(self):
+        return Point(self.pt1.x, self.pt2.y)
+
+    @property
+    def bottomleft(self):
+        return self.pt1
+
+    @property
+    def topright(self):
+        return self.pt2
+
+    @property
+    def bottomright(self):
+        return Point(self.pt2.x, self.pt1.y)
--- lekcja08/test_rectangles.py
+++ lekcja08/test_rectangles.py
@@ -1,58 +1,90 @@
-import unittest
-import rectangles as r
+import pytest
+from lekcja06.points import Point
 from rectangles import Rectangle
 
 
-class MyTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.r1 = r.Rectangle(0, 0, 3, 2)
-        self.r2 = r.Rectangle(1, 1, 5, 4)
-        self.r3 = r.Rectangle(0, 0, 3, 2)
-
-    def test_str(self):
-        self.assertEqual(str(self.r1), "[(0, 0), (3, 2)]")
-        self.assertEqual(str(self.r2), "[(1, 1), (5, 4)]")
-
-    def test_repr(self):
-        self.assertEqual(repr(self.r1), "Rectangle(0, 0, 3, 2)")
-        self.assertEqual(repr(self.r2), "Rectangle(1, 1, 5, 4)")
-
-    def test_eq(self):
-        self.assertEqual(self.r1, self.r3)
-        self.assertNotEqual(self.r1, self.r2)
-
-    def test_ne(self):
-        self.assertNotEqual(self.r1, self.r2)
-
-    def test_center(self):
-        self.assertEqual(self.r1.center(), r.Point(3 / 2, 1))
-        self.assertEqual(self.r2.center(), r.Point(3, 5 / 2))
-
-    def test_area(self):
-        self.assertEqual(self.r1.area(), 6)
-        self.assertEqual(self.r2.area(), 12)
-
-    def test_move(self):
-        self.r1.move(1, 1)
-        self.assertEqual(self.r1, Rectangle(1, 1, 4, 3))
-        self.r2.move(-1, -1)
-        self.assertEqual(self.r2, Rectangle(0, 0, 4, 3))
-
-    def test_intersection(self):
-        self.assertEqual(self.r1.intersection(self.r2), Rectangle(1, 1, 3, 2))
-        self.assertEqual(self.r1.intersection(self.r3), self.r1)
-
-    def test_cover(self):
-        self.assertEqual(self.r1.cover(self.r2), Rectangle(0, 0, 5, 4))
-        self.assertEqual(self.r1.cover(self.r3), self.r1)
-
-    def test_make4(self):
-        self.assertEqual(self.r1.make4()[0], Rectangle(0, 0, 1.5, 1))
-        self.assertEqual(self.r1.make4()[1], Rectangle(1.5, 0, 3, 1))
-        self.assertEqual(self.r1.make4()[2], Rectangle(0, 1, 1.5, 2))
-        self.assertEqual(self.r1.make4()[3], Rectangle(1.5, 1, 3, 2))
+@pytest.fixture
+def rectangles():
+    r1 = Rectangle(0, 0, 3, 2)
+    r2 = Rectangle(1, 1, 5, 4)
+    r3 = Rectangle(0, 0, 3, 2)
+    return r1, r2, r3
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_str(rectangles):
+    r1, r2, r3 = rectangles
+    assert str(r1) == "[(0, 0), (3, 2)]"
+    assert str(r2) == "[(1, 1), (5, 4)]"
+    assert str(r3) == str(r1)
+
+
+def test_repr(rectangles):
+    r1, r2, r3 = rectangles
+    assert repr(r1) == "Rectangle(0, 0, 3, 2)"
+    assert repr(r2) == "Rectangle(1, 1, 5, 4)"
+    assert repr(r3) == repr(r1)
+
+
+def test_eq(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1 == r3
+
+
+def test_ne(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1 != r2
+
+
+def test_center(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1.center == Point(1.5, 1)
+    assert r2.center == Point(3, 2.5)
+
+
+def test_area(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1.area == 6
+    assert r2.area == 12
+
+
+def test_move(rectangles):
+    r1, r2, r3 = rectangles
+    r1.move(1, 1)
+    assert r1 == Rectangle(1, 1, 4, 3)
+    r2.move(-1, -1)
+    assert r2 == Rectangle(0, 0, 4, 3)
+
+
+def test_intersection(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1.intersection(r2) == Rectangle(1, 1, 3, 2)
+    assert r1.intersection(r3) == r1
+
+
+def test_cover(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1.cover(r2) == Rectangle(0, 0, 5, 4)
+    assert r1.cover(r3) == r1
+
+
+def test_make4(rectangles):
+    r1, r2, r3 = rectangles
+    parts = r1.make4()
+    assert parts[0] == Rectangle(0, 0, 1.5, 1)
+    assert parts[1] == Rectangle(1.5, 0, 3, 1)
+    assert parts[2] == Rectangle(0, 1, 1.5, 2)
+    assert parts[3] == Rectangle(1.5, 1, 3, 2)
+
+
+def test_virtual_attributes(rectangles):
+    r1, r2, r3 = rectangles
+    assert r1.top == 2
+    assert r1.bottom == 0
+    assert r1.left == 0
+    assert r1.right == 3
+    assert r1.width == 3
+    assert r1.height == 2
+    assert r1.topleft == Point(0, 2)
+    assert r1.bottomleft == Point(0, 0)
+    assert r1.topright == Point(3, 2)
+    assert r1.bottomright == Point(3, 0)
